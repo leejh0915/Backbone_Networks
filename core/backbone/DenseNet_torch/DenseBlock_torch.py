cc28006127b0153c1657d4ef8498f547bee42c3e
leejh0915 <programmer.leejh0915@gmail.com>
2022-08-16 13:02:16 +0900

    22/08/16 pytorch DenseNet 구현 완료

--- core/backbone/DenseNet_torch/DenseBlock_torch.py
+++ core/backbone/DenseNet_torch/DenseBlock_torch.py
@@ -3,7 +3,49 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-#k=12 l=100
+class BottleNeckBlock(nn.Module):
+    def __init__(self, nin, growth_rate, kernel_size, drop_rate):
+        super(BottleNeckBlock, self).__init__()
+        self.bn_1 = nn.BatchNorm2d(nin)
+        self.relu_1 = nn.ReLU(True)
+        self.conv_1 = nn.Conv2d(in_channels=nin, out_channels=growth_rate * 4, kernel_size=kernel_size, stride=1,
+                                padding=0)  # 4*k
+
+        self.bn_2 = nn.BatchNorm2d(growth_rate * 4)
+        self.relu_2 = nn.ReLU(True)
+        self.conv_2 = nn.Conv2d(in_channels=growth_rate * 4, out_channels=growth_rate, kernel_size=kernel_size,
+                                stride=1, padding=0)  # k
+
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        input = x
+
+        x = self.bn_1(input)
+        x = self.relu_1(x)
+        x = self.conv_1(x)
+
+        x = self.bn_2(x)
+        x = self.relu_2(x)
+        x = self.conv_2(x)
+
+        x = F.dropout(x, p=self.drop_rate, training=self.training)  # self.training은 nn.Module에서...
+        x = torch.cat([x, input], dim=1)
+
+        return x
+
+class DenseBlock_test(nn.Sequential):
+    def __init__(self, nin, growth_rate, kernel_size, iter_num, drop_rate=0.2):
+        super(DenseBlock_test, self).__init__()
+        #self.iter = iter_num
+
+        #input값만 변경해줄것
+        for i in range(iter_num):
+            in_channels = int(i * growth_rate + nin)
+            self.add_module('DenseBlock_{}'.format(i), BottleNeckBlock(nin=in_channels, growth_rate=growth_rate,
+                                                                       kernel_size=kernel_size, drop_rate=drop_rate))
+
+
 
 class DenseBlock(nn.Module):
     def __init__(self, nin, growth_rate, kernel_size, iter_num, drop_rate=0.2):
@@ -18,30 +60,56 @@ class DenseBlock(nn.Module):
 
         self.bn_2 = nn.BatchNorm2d(growth_rate*4)
         self.relu_2 = nn.ReLU(True)
-        self.conv_2 = nn.Conv2d(in_channels=growth_rate*4, out_channels=growth_rate, kernel_size=kernel_size, stride=1, padding=0) #k
+        self.conv_2 = nn.Conv2d(in_channels=growth_rate*4, out_channels=growth_rate, kernel_size=kernel_size*3, stride=1, padding=0) #k
 
 
     def forward(self, x):
-        print('shape: {}'.format(x.shape))
-        print('nin: {}'.format(self.nin))
+        input = x
 
-        for i in range(self.iter_num):
-            input = x
+        x = self.bn_1(input)
+        x = self.relu_1(x)
+        x = self.conv_1(x)
 
-            x = self.bn_1(input)
-            x = self.relu_1(x)
-            x = self.conv_1(x)
+        x = self.bn_2(x)
+        x = self.relu_2(x)
+        x = self.conv_2(x)
 
-            x = self.bn_2(x)
-            x = self.relu_2(x)
-            x = self.conv_2(x)
-
-            x = F.dropout(x, p=self.drop_rate, training=self.training) #self.training은 nn.Module에서...
-            x = torch.cat([x, input], dim=1)
-
-            print('{}차 반복끝'.format(i+1))
+        x = F.dropout(x, p=self.drop_rate, training=self.training) #self.training은 nn.Module에서...
+        x = torch.cat([x, input], dim=1)
 
         return x
+
+
+    # def __init__(self, nin, growth_rate, kernel_size, iter_num, drop_rate=0.2):
+    #     super(DenseBlock, self).__init__()
+    #     self.drop_rate = drop_rate
+    #     self.iter_num = iter_num
+    #     self.nin = nin
+    #
+    #     self.bn_1 = nn.BatchNorm2d(nin)
+    #     self.relu_1 = nn.ReLU(True)
+    #     self.conv_1 = nn.Conv2d(in_channels=nin, out_channels=growth_rate*4, kernel_size=kernel_size, stride=1, padding=0) #4*k
+    #
+    #     self.bn_2 = nn.BatchNorm2d(growth_rate*4)
+    #     self.relu_2 = nn.ReLU(True)
+    #     self.conv_2 = nn.Conv2d(in_channels=growth_rate*4, out_channels=growth_rate, kernel_size=kernel_size, stride=1, padding=0) #k
+    #
+    #
+    # def forward(self, x):
+    #     input = x
+    #
+    #     x = self.bn_1(input)
+    #     x = self.relu_1(x)
+    #     x = self.conv_1(x)
+    #
+    #     x = self.bn_2(x)
+    #     x = self.relu_2(x)
+    #     x = self.conv_2(x)
+    #
+    #     x = F.dropout(x, p=self.drop_rate, training=self.training) #self.training은 nn.Module에서...
+    #     x = torch.cat([x, input], dim=1)
+    #
+    #     return x
 
 class TransitionLayer(nn.Module):
     def __init__(self, nin, theta=0.5, kernel_size=1):
--- core/backbone/DenseNet_torch/DenseNet_torch.py
+++ core/backbone/DenseNet_torch/DenseNet_torch.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import numpy as np
 
 from core.backbone.DenseNet_torch.DenseBlock_torch import TransitionLayer
-from core.backbone.DenseNet_torch.DenseBlock_torch import DenseBlock
+from core.backbone.DenseNet_torch.DenseBlock_torch import DenseBlock_test
 
 class DenseNet121(nn.Module):
     def __init__(self):
@@ -19,26 +19,20 @@ class DenseNet121(nn.Module):
         self.dense3_oc = int(self.dense2_oc * 0.5 + self.growth_rate * self.iter[2])
         self.dense4_oc = int(self.dense3_oc * 0.5 + self.growth_rate * self.iter[3])
 
-        print('dense0: {}'.format(self.dense0_oc))
-        print('dense1: {}'.format(self.dense1_oc))
-        print('dense2: {}'.format(self.dense2_oc))
-        print('dense3: {}'.format(self.dense3_oc))
-        print('dense4: {}'.format(self.dense4_oc))
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.dense0_oc, kernel_size=7, stride=2, padding=4)
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.dense0_oc, kernel_size=7, stride=2)
-        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.DenseBlock1 = DenseBlock(nin=self.dense0_oc, growth_rate=self.growth_rate,
-                                      kernel_size=1, iter_num=self.iter[0])
+        self.DenseBlock1 = DenseBlock_test(nin=self.dense0_oc, growth_rate=self.growth_rate,
+                                           kernel_size=1, iter_num=self.iter[0])
         self.TransitionLayer1 = TransitionLayer(nin=self.dense1_oc)
-        self.DenseBlock2 = DenseBlock(nin=int(self.dense1_oc * 0.5), growth_rate=self.growth_rate,
-                                      kernel_size=1, iter_num=self.iter[1])
+        self.DenseBlock2 = DenseBlock_test(nin=int(self.dense1_oc * 0.5), growth_rate=self.growth_rate,
+                                           kernel_size=1, iter_num=self.iter[1])
         self.TransitionLayer2 = TransitionLayer(nin=self.dense2_oc)
-        self.DenseBlock3 = DenseBlock(nin=int(self.dense2_oc * 0.5), growth_rate=self.growth_rate,
-                                      kernel_size=1, iter_num=self.iter[2])
+        self.DenseBlock3 = DenseBlock_test(nin=int(self.dense2_oc * 0.5), growth_rate=self.growth_rate,
+                                           kernel_size=1, iter_num=self.iter[2])
         self.TransitionLayer3 = TransitionLayer(nin=self.dense3_oc)
-        self.DenseBlock4 = DenseBlock(nin=int(self.dense3_oc * 0.5), growth_rate=self.growth_rate,
-                                      kernel_size=1, iter_num=self.iter[3])
+        self.DenseBlock4 = DenseBlock_test(nin=int(self.dense3_oc * 0.5), growth_rate=self.growth_rate,
+                                           kernel_size=1, iter_num=self.iter[3])
         self.FCLayer = nn.Linear(self.dense4_oc, 10)
 
 
@@ -52,6 +46,9 @@ class DenseNet121(nn.Module):
         x = self.DenseBlock3(x)
         x = self.TransitionLayer3(x)
         x = self.DenseBlock4(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(x.size(0), -1)
+
         x = self.FCLayer(x)
 
         return x
